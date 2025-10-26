ea87a9cce17a15aa7dec1bf43ba21dd4add7fe90
epercar444 <epercar444@g.educaand.es>
2025-10-26 18:38:16 +0100

    primeras prueba con node/npm

---
+++ nodeEvaPerez/index.js
@@ -0,0 +1,6 @@
+import funnyWords from "funny-words";
+import { v4 as uuidv4 } from "uuid";
+
+console.log(funnyWords("Entrar a ver mis notas es un deporte extremo no reconocido por la ONU."));
+console.log(uuidv4()); 
+
